11a2bc38a6e2a90010015585fe165bc58c0ac990
Anyi <daniela.09mh@gmail.com>
2026-09-11 19:36:44 -0500

    test yml1

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,7 @@ globals: true,
 include: ["test/**/*.spec.ts"],
 coverage: {
 provider: "v8",
-reporter: ["text", "html"],
+reporter: ["text", "html", "lcov"],
 include: ["src/**/*.ts"],
 exclude: [
 ...defaultExclude,
